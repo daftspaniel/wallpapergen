919eb724174cc4bd397566c138b150a27d7cb8a7
Davy Mitchell <daftspaniel@gmail.com>
2021-11-16 08:04:38 +0000

    Add shift to colour class. Palette generation adjustments.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import os from 'os'
 
 import { savePNG } from './lib/io.js'
 import { Gfx } from './lib/gfx.js'
-import { Colour, Stock } from './lib/colour.js'
+import { mix, Colour, Stock } from './lib/colour.js'
 import { getWord, getFont } from './lib/words.js'
 
 const width = 1920
@@ -16,32 +16,45 @@ const g = new Gfx(context, width, height)
 const date = new Date()
 let day = date.getDate()
 const shade = date.getDate() + 100
+
 const palette = {
-  background: new Colour(0, 0, shade),
-  grid: new Colour(0, shade, 0),
+  background: mix(0, 0, shade),
+  grid: mix(0, shade, 0),
+  text: mix(255, 255, 255),
 }
 console.log(date)
 console.log('OS:\t' + os.platform())
+
+palette.text = palette.background
+  .clone()
+  .invert()
+  .lighten(3 * day)
+
+palette.grid = palette.background
+  .clone()
+  .lighten(12 * day)
 
 g.cls(palette.background)
 
 if (day % 2 === 0) {
   for (let h = day; h < height - day; h += day * 2) {
     g.drawHStripe(palette.grid, h, width - day * 2, 4, day)
+    palette.grid.lighten(-2)
   }
 }
 
 if (day % 4 === 0) {
   for (let h = day; h < width - day; h += day * 2) {
     g.drawVStripe(palette.grid, h, height - day * 2, 4, day)
+    palette.grid.lighten(-4)
   }
 }
 
 const word = getWord()
-context.font = getFont(111)
+context.font = getFont(151)
 
 const textWidth = context.measureText(word).width
 
-g.drawText(Stock.white, (width - textWidth) / 2, 555, word)
+g.drawText(palette.text, (width - textWidth) / 2, 555, word)
 
 savePNG(canvas, 'out\\test.png')
