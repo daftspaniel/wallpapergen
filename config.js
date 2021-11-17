f7e24aadf8182f2f103203e2c37d101976415316
Davy Mitchell <daftspaniel@gmail.com>
2021-11-17 08:00:47 +0000

    Add timing. Introduce config.

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+  width: 1920,
+  height: 1200,
+  filename: 'out\\test.png',
+}
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import c from 'canvas'
 import os from 'os'
+import fs from 'fs'
 
 import { savePNG } from './lib/general/io.js'
 import { Gfx } from './lib/general/gfx.js'
 import { generatePalette } from './lib/wallpaper/palette.js'
 import { getWord, getFont } from './lib/wallpaper/words.js'
+import { config } from './config.js'
 
-const width = 1920
-const height = 1200
+const startTime = performance.now()
 
-const canvas = c.createCanvas(width, height)
+const canvas = c.createCanvas(config.width, config.height)
 const context = canvas.getContext('2d')
-const g = new Gfx(context, width, height)
+const g = new Gfx(context, config.width, config.height)
 
 const date = new Date()
 let day = date.getDate()
 
 // Verbose details
-console.log('Wallpaper Generator: ',date)
+console.log('Wallpaper Generator: ', date)
 console.log('OS:\t' + os.platform())
 
 // Palette generation
@@ -28,14 +29,14 @@ g.cls(palette.background)
 
 if (day % 2 === 0) {
   for (let h = day; h < height - day; h += day * 2) {
-    g.drawHStripe(palette.grid, h, width - day * 2, 4, day)
+    g.drawHStripe(palette.grid, h, config.width - day * 2, 4, day)
     palette.grid.lighten(-2)
   }
 }
 
 if (day % 4 === 0) {
   for (let h = day; h < width - day; h += day * 2) {
-    g.drawVStripe(palette.grid, h, height - day * 2, 4, day)
+    g.drawVStripe(palette.grid, h, config.height - day * 2, 4, day)
     palette.grid.lighten(-4)
   }
 }
@@ -45,6 +46,10 @@ context.font = getFont(151)
 
 const textWidth = context.measureText(word).width
 
-g.drawText(palette.text, (width - textWidth) / 2, 555, word)
+g.drawText(palette.text, (config.width - textWidth) / 2, 555, word)
 
-savePNG(canvas, 'out\\test.png')
+savePNG(canvas, config.filename)
+const endTime = performance.now()
+
+console.log(`Saved to : ${config.filename} ${fs.statSync(config.filename).size} bytes`)
+console.log('Time taken: ' + (endTime - startTime))
