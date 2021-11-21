cd90f0e20fcd3f37c6a9748e5b3e2a951322e5b5
Davy Mitchell <daftspaniel@gmail.com>
2021-11-21 09:44:30 +0000

    Add count CLA and make logging config.

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ export const config = {
   log: true,
   width: 1920,
   height: 1200,
-  filename: 'out\\test.png',
+  filename: 'out\\test',
   verticalStripes: true,
   horizontalStripes: true,
   text: true,
+  logging: { palette: false, save: false },
 }
--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-import fs from 'fs'
 import os from 'os'
 import { Logger } from './lib/general/logger.js'
+import { Command } from 'commander/esm.mjs'
 
 import { config } from './config.js'
 import { generateWallpaper } from './lib/wallpaper/wallpaper.js'
 
-export const log = Logger(config).log
+const log = Logger(config).log
+const program = new Command()
 
+program.option(
+  '-c, --count <number>',
+  'number of wallpaper images to produce',
+  1
+)
+
+program.parse(process.argv)
+
+const { count } = program.opts()
 const startTime = performance.now()
-const date = new Date()
+let date = new Date()
 
 // App and Platform details
 log('Wallpaper Generator: ' + date)
 
 log('OS:\t' + os.platform())
+log('Please wait...')
 
-generateWallpaper(log, config, date)
+for (let k = 0; k < count; k++) {
+  generateWallpaper(log, config, date, k)
+  date.setDate(date.getDate() + 1)
+}
 
 const endTime = performance.now()
 
 log()
-log(`Saved to : ${config.filename} ${fs.statSync(config.filename).size} bytes`)
 log('Time taken: ' + (endTime - startTime))
